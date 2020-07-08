c7bc50c25a20f33991ca2d7a03c1a3366ee86dff
ashishtayal89 <ashishtayal14@gmail.com>
2020-07-09 02:04:35 +0530

    Update chapter 6

--- client/src/chapters/chapter_6/app.js
+++ client/src/chapters/chapter_6/app.js
@@ -3,11 +3,16 @@ import ReactDOM from "./reactDom";
 
 function Counter() {
   const [state, setState] = useState(1);
-  return <h1 onclick={() => setState(c => c + 1)}>Count: {state}</h1>;
+  return <h4 onclick={() => setState(c => c + 1)}>Count: {state}</h4>;
 }
 
 const renderApp = () => {
-  const element = <Counter />;
+  const element = (
+    <div>
+      <h2 title="child">Chapter 6 (Hooks)</h2>
+      <Counter />
+    </div>
+  );
   ReactDOM.render(element, document.getElementById("chapter6"));
 };
 
